56c394a5e6bc94dec2ec75554ba987b3e6112b37
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2025-12-08 16:31:39 +0900

    feat(score): 기본 점수 출력 기능

--- lib/score_advanced.dart
+++ lib/score_advanced.dart
@@ -1,1 +1,25 @@
+import 'dart:io';
 
+class Score {
+  int score;
+  Score(this.score);
+
+  void showInfo() {
+    print("점수: $score");
+  }
+}
+
+class StudentScore extends Score {
+  String name;
+  StudentScore(this.name, int score) : super(score);
+  @override
+  void showInfo() {
+    super.showInfo();
+    print("추가된 내용");
+  }
+}
+
+void main() {
+  var s = Score(90);
+  s.showInfo();
+}
