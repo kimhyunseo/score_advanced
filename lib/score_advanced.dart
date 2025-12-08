03ce2a159a8005970d0829829ad3ae570fccee79
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2025-12-08 17:36:07 +0900

    feat(score): loadStudentData로 student.txt 불러오기 기능 추가

--- lib/score_advanced.dart
+++ lib/score_advanced.dart
@@ -9,20 +9,42 @@ class Score {
   }
 }
 
+void loadStudentData(String filePath) {
+  try {
+    final file = File(filePath);
+    final lines = file.readAsLinesSync();
+
+    for (var line in lines) {
+      final parts = line.split(',');
+      if (parts.length != 2) throw FormatException('잘못된 데이터 형식: $line');
+
+      String name = parts[0];
+      int score = int.parse(parts[1]);
+    }
+  } catch (e) {
+    print("학생 데이터를 불러오는 데 실패했습니다: $e");
+    exit(1);
+  }
+  return;
+}
+
 class StudentScore extends Score {
   String name;
   StudentScore(this.name, int score) : super(score);
   @override
   void showInfo() {
-    super.showInfo();
-    print("추가된 내용");
+    print("이름: $name, 점수: $score");
   }
 }
 
-void main() {
-  var s = Score(90);
-  s.showInfo();
+var students = 'lib/students.txt';
 
-  var ss = StudentScore("홍길동", 68);
-  ss.showInfo();
+void main() {
+  // var s = Score(90);
+  // s.showInfo();
+
+  // var ss = StudentScore("홍길동", 68);
+  // ss.showInfo();
+
+  loadStudentData(students);
 }
